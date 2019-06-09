d58f3aa9c78a1e8d8fd1970504d4afbaf879dbcc
Levchenkoolga2001 <49477103+Levchenkoolga2001@users.noreply.github.com>
2019-06-09 22:58:28 +0300

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#include <cctype>
+#include <algorithm>
+using namespace std;
+
+int main(){
+	string inp;
+	getline(cin, inp);
+	int a, e, i, o, u;
+	a = 0;
+	e = 0;
+	i = 0;
+	o = 0;
+	u = 0;
+	for (int c = 0; c < inp.size();c++){
+		inp.at(c) = tolower(inp.at(c));
+		switch(inp.at(c)){
+			case 'a': ++a; break;
+			case 'e': ++e; break;
+			case 'i': ++i; break;
+			case 'o': ++o; break;
+			case 'u': ++u; break;
+		}
+	}
+	cout << a << " " << e << " " << i << " " << o << " " << u << " " << endl;
+	return 0;
+}
