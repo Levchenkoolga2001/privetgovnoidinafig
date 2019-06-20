74a2a47bfe2b9a8e7dc380f60de95ca2a9b6982c
Levchenkoolga <Levchenkoolga3020@icloud.com>
2019-06-20 12:47:21 +0300

    Посчитала сколько гласных в строчке

--- 1.cpp
+++ 1.cpp
@@ -1,21 +1,16 @@
-#include <iostream>
-#include <string>
-#include <cctype>
-#include <algorithm>
-using namespace std;
+#include <iostream> //для оператора cout
+#include <string> //для использования букв
+#include <cctype> //для функции tolower
+using namespace std; //пространство имен std
 
 int main(){
-	string inp;
-	getline(cin, inp);
-	int a, e, i, o, u;
-	a = 0;
-	e = 0;
-	i = 0;
-	o = 0;
-	u = 0;
-	for (int c = 0; c < inp.size();c++){
-		inp.at(c) = tolower(inp.at(c));
-		switch(inp.at(c)){
+	string inp; //создаем массивad для строчки с буквами
+	getline(cin, inp); //читаю всю строчку с буквами
+	int a=0, e=0, i=0, o=0, u=0; //создаю переменные, куда буду записывать количество аналогичных букв
+	for (int c = 0; c < inp.size();c++){  //inp.size()-функция, возвращающая количество знаков в массиве (строчке)
+		inp.at(c) = tolower(inp.at(c));  //функция tolower делает из больших букв маленькие 
+                                         //и записываю это преобразование в тот же массив (at.(c)-обращение по индексу)
+		switch(inp.at(c)){  //ищу совпадения с гласной и перезаписываю в соответственную переменную
 			case 'a': ++a; break;
 			case 'e': ++e; break;
 			case 'i': ++i; break;
@@ -23,6 +18,7 @@ int main(){
 			case 'u': ++u; break;
 		}
 	}
-	cout << a << " " << e << " " << i << " " << o << " " << u << " " << endl;
+	cout <<"a=" << a << " " <<"e=" << e << " " <<"i=" << i << " " <<"o=" << o << " " <<"u=" << u << " " << endl; 
+    // вывожу переменные, в которые я записала результаты
 	return 0;
 }
