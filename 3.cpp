75e0d797b9e2b1b86c5155757eae28d3dfe073a9
Levchenkoolga <Levchenkoolga3020@icloud.com>
2019-06-20 13:20:11 +0300

    Сделала из больших гласных букв маленькие

--- 3.cpp
+++ 3.cpp
@@ -1,17 +1,16 @@
-#include <iostream>
-#include <string>
-#include <cctype>
-#include <algorithm>
-using namespace std;
+#include <iostream>//для оператора cout
+#include <string>  //для использования букв
+#include <cctype>  //для функции tolower
+using namespace std;//пространство имен std
 
 int main(){
-	string inp;
-	getline(cin, inp);
-	for (int c = 0; c < inp.size();c++){
-		char m = inp.at(c);
-		if (m == 'A' || m == 'E' || m =='I' || m == 'O' || m == 'U')
-			inp.at(c) = tolower(inp.at(c));
+	string inp;  //создаем массивad для строчки с буквами
+	getline(cin, inp);   //читаю всю строчку с буквами
+	for (int c = 0; c < inp.size();c++){  //inp.size()-функция, возвращающая количество знаков в массиве (строчке)
+		char m = inp.at(c);  //создаю переменную, в которой буду проверять символы
+		if (m == 'A' || m == 'E' || m =='I' || m == 'O' || m == 'U') //проверяю символы на наличие гласных заглавных
+			inp.at(c) = tolower(inp.at(c));  //делаю гласные заглавные маленькими
 	}
-	cout << inp << endl;
+	cout << inp << endl;  //вывожу строчку
 	return 0;
 }
